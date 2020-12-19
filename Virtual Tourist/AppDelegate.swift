a9d9d2953fdd138389a5137ed37df17fd3fada21
Ivan Kodrnja <ikodrnja@gmail.com>
2020-12-19 19:49:18 +0100

    edited the copyright notice

--- Virtual Tourist/AppDelegate.swift
+++ Virtual Tourist/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Virtual Tourist
 //
 //  Created by Ivan Kodrnja on 12/09/15.
-//  Copyright (c) 2015 Ivan Kodrnja. All rights reserved.
+//  Copyright (c) 2015-2021 Ivan Kodrnja. All rights reserved.
 //
 
 import UIKit
